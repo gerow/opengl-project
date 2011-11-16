14613101baef51fd6f38c6e715d0dc2af712c704
gerow <devnull@localhost>
2011-11-15 19:22:43 -0800

    Forgot to include...

---
+++ src/sceneobjects/DynamicSceneObject.java
@@ -0,0 +1,73 @@
+package sceneobjects;
+
+import glproject.Mesh;
+import glproject.SceneObject;
+import glproject.Vector3d;
+import glproject.World;
+
+public abstract class DynamicSceneObject implements SceneObject {
+    protected World world;
+    protected Mesh mesh;
+    
+    private Vector3d location = new Vector3d(0, 0, 0);
+    private Vector3d velocity = new Vector3d(0, 0, 0);
+    private Vector3d acceleration = new Vector3d(0, 0, 0);
+    
+    private Vector3d rotation = new Vector3d(0, 0, 0);
+    
+    public DynamicSceneObject(Mesh mesh) {
+	this.mesh = mesh;
+    }
+    
+    public void setMesh(Mesh mesh) {
+	this.mesh = mesh;
+	this.mesh.translation = this.location;
+    }
+    
+    public void initialize(World world) {
+	this.world = world;
+    }
+    
+    public void velocityMove() {
+	this.location = this.location.add(this.velocity);
+	this.mesh.translation = this.location;
+    }
+    
+    public void accelerationMove() {
+	this.velocity = this.velocity.add(this.acceleration);
+	this.velocityMove();
+    }
+    
+    public void setLocation(Vector3d location) {
+	this.location = location;
+	this.mesh.translation = this.location;
+    }
+    
+    public Vector3d getLocation() {
+	return this.location;
+    }
+    
+    public void setVelocity(Vector3d velocity) {
+	this.velocity = velocity;
+    }
+    
+    public Vector3d getVelocity() {
+	return this.velocity;
+    }
+    
+    public void setAcceleration(Vector3d acceleration) {
+	this.acceleration = acceleration;
+    }
+    
+    public Vector3d getAcceleration() {
+	return this.acceleration;
+    }
+    
+    public void setRotation(Vector3d rotation) {
+	this.rotation = rotation;
+    }
+    
+    public Vector3d getRotation() {
+	return this.rotation;
+    }
+}
---
+++ src/sceneobjects/GravityMover.java
@@ -0,0 +1,52 @@
+package sceneobjects;
+
+import java.util.ArrayList;
+
+import glproject.Mesh;
+import glproject.Vector3d;
+
+public class GravityMover extends DynamicSceneObject {
+    public static float G = (float) (6 * Math.pow(10, -11));
+    private float mass = 0;
+    private ArrayList<GravityMover> interactingObjects = new ArrayList<GravityMover>();
+
+    public GravityMover(Mesh mesh, float mass) {
+	super(mesh);
+	this.mass = mass;
+    }
+
+    public void step() {
+	this.gravityMove();
+    }
+
+    public void applyForce(Vector3d force) {
+	Vector3d changeInAcceleration = force.divide(this.mass);
+	this.setAcceleration(this.getAcceleration().add(changeInAcceleration));
+    }
+
+    public void gravityMove() {
+	for (GravityMover g : this.interactingObjects) {
+	    float m1, m2, r;
+
+	    m1 = this.getMass();
+	    m2 = g.getMass();
+	    r = this.getLocation().vectorTo(g.getLocation()).magnitude();
+
+	    float forceMagnitude = (float) (GravityMover.G * (m1 * m2) / Math
+		    .pow(r, 2));
+
+	    Vector3d force = this.getAcceleration()
+		    .vectorTo(g.getAcceleration()).normalize()
+		    .multiply(forceMagnitude);
+	    this.applyForce(force);
+	}
+    }
+
+    public float getMass() {
+	return this.mass;
+    }
+
+    public void setMass(float mass) {
+	this.mass = mass;
+    }
+}
